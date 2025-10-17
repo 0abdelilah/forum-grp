557ebde95d8fdda5ae5a6371ac9f8b6e256427f5
0abdelilah <iboatinc@gmail.com>
2025-10-17 16:21:06 +0200

    gg

--- frontend/templates/static/js/comments.js
+++ frontend/templates/static/js/comments.js
@@ -1,24 +1,15 @@
 function ShowComments(postId) {
-
     document.getElementById('popup').style.display = 'block';
     document.getElementById('overlay').style.display = 'block';
 
     const url = '/api/comments?postId=' + postId
     fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Fetch error:', error))
-    
-    // open a popup, displaying comments
-}
-
-function closePopup() {
-  document.getElementById('popup').style.display = 'none';
-  document.getElementById('overlay').style.display = 'none';
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Fetch error:', error))
 }
 
 function CreateComment(btn) {
-
     const postId = btn.closest('.post_comments').dataset.id;
     const content = btn.closest('.post_comments').querySelector('#cmntarea').value;
 
@@ -35,4 +26,9 @@ function CreateComment(btn) {
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(err => console.error(err));
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
 }
